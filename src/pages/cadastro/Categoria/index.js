98e28e362f71b3c86c6c2167d42882f8314e4aaa
laianehermes <laiane.sp@hotmail.com>
2020-07-30 21:59:39 -0300

    atualizar o package e melhoria na url do serviço

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,18 +31,20 @@ function CadastroCategoria() {
   // ============
 
   useEffect(() => {
+    const URL = 'http://localhost:8080/categorias'; 
+
     if(window.location.href.includes('localhost')) {
       const URL = 'http://localhost:8080/categorias'; 
-      fetch(URL)
-       .then(async (respostaDoServer) =>{
-        if(respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setCategorias(resposta);
-          return; 
-        }
-        throw new Error('Não foi possível pegar os dados');
-       })
     }    
+    fetch(URL)
+     .then(async (respostaDoServer) =>{
+      if(respostaDoServer.ok) {
+        const resposta = await respostaDoServer.json();
+        setCategorias(resposta);
+        return; 
+      }
+      throw new Error('Não foi possível pegar os dados');
+     })
   }, []);
 
   return (
